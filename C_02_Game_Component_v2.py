7e21146e1094e4c577c7481eeaff51ef3f011b7d
sylas08 <lundonk2025@masseyhigh.school.nz>
2026-03-04 09:23:55 +1300

    Integrated colour retrieval

---
+++ C_02_Game_Component_v2.py
@@ -0,0 +1,197 @@
+import csv
+import random
+from tkinter import *
+from functools import partial  # To prevent unwanted windows
+
+
+
+# helper functions go here
+def get_colours():
+    """
+    Retrieve colours from csv file
+    :return: list of colours which where each list item has the colour name, associated score and foreground colour for the text
+    """
+
+    file = open("00_colour_list_hex_v3.csv", "r")
+    all_colours = list(csv.reader(file, delimiter=","))
+    file.close()
+
+    # remove the first row
+    all_colours.pop(0)
+
+    return all_colours
+
+
+
+def get_round_colours():
+    """
+    Choose four colours from larger list ensuring that the scores are all different.
+    :return: list of colours and score to beat (median of scores)
+    """
+
+    all_colour_list = get_colours()
+
+    round_colours = []
+    colour_scores = []
+
+    # loop until we have four colours with different scores...
+    while len(round_colours) < 4:
+        potential_colour = random.choice(all_colour_list)
+
+        # colour scores are being read as a string,
+        # change them to an integer to compare / when adding to score list.
+        if potential_colour[1] not in colour_scores:
+            round_colours.append(potential_colour)
+
+            # make score an integer and add it to the list of scores
+            colour_scores.append(potential_colour[1])
+
+    # change scores to integers...
+    int_scores = [int(x) for x in colour_scores]
+
+    # Get median score / target score
+    int_scores.sort()
+    median = (int_scores[1] + int_scores[2]) / 2
+    median = round_ans(median)
+
+    return round_colours, median
+
+
+def rounds_ans(val):
+    """
+    Rounds numbers to nearest integer
+    :param val: number to be rounded.
+    :return: Rounded number (an integer)
+    """
+    var_rounded = (val * 2 + 1) // 2
+    raw_rounded = "{:.0f}".format(var_rounded)
+    return int(raw_rounded)
+
+
+
+
+def round_ans(val):
+    """
+    Rounds numbers to nearest integer
+    :param val: number to be rounded.
+    :return: Rounded number (an integer)
+    """
+    var_rounded = (val * 2 + 1) // 2
+    raw_rounded = "{:.0f}".format(var_rounded)
+    return int(raw_rounded)
+
+
+
+# Classes start here
+class StartGame:
+    """
+    Initial Game interface (asks users how many rounds they
+    would like to play)
+    """
+
+    def __init__(self):
+        """
+        Gets number of rounds from user
+        """
+
+        self.start_frame = Frame(padx=10, pady=10)
+        self.start_frame.grid()
+
+        # Create play button...
+        self.play_button = Button(self.start_frame, font=("Arial", 16, "bold"),
+                                  fg="#FFFFFF", bg="#0057D8", text="Play", width=10,
+                                  command=self.check_rounds)
+        self.play_button.grid(row=0, column=1)
+
+
+    def check_rounds(self):
+        """
+        Checks users have entered 1 or more rounds
+        """
+
+        Play(5)
+        # Hide root window (ie: hide rounds choice window)
+        root.withdraw()
+
+
+
+class Play:
+    """
+    Interface for play the Colour Quest Game
+    """
+
+    def __init__(self, how_many):
+        self.play_box = Toplevel()
+
+        self.game_frame = Frame(self.play_box)
+        self.game_frame.grid(padx=10, pady=10)
+
+        # body font for most labels...
+        body_font = ("Arial", 12)
+
+        # List for label details (text | font | background | row)
+        play_labels_list = [
+            ["Round # of #", ("Arial", "16", "bold"), None, 0],
+            ["Score to beat: #", body_font, "#FFF2CC", 1],
+            ["Choose a colour below.  Good luck. 🍀", body_font, "#D5E8D4", 2],
+            ["You chose, result", body_font, "#D5E8D4", 4]
+        ]
+
+        play_labels_ref = []
+        for item in play_labels_list:
+            self.make_label = Label(self.game_frame, text=item[0], font=item[1],
+                                    bg=item[2], wraplength=300, justify="left")
+            self.make_label.grid(row=item[3], pady=10, padx=10)
+
+            play_labels_ref.append(item)
+
+        # Retrieve Labels so they can be configured later
+        self.heading_label = play_labels_ref[0]
+        self.target_label = play_labels_ref[1]
+        self.results_label = play_labels_ref[3]
+
+        # set up colour buttons...
+        self.colour_frame = Frame(self.game_frame)
+        self.colour_frame.grid(row=3)
+
+        # create four buttons in a 2 x 2 grid
+        for item in range(0, 4):
+            self.colour_button = Button(self.colour_frame, font=("Arial", 12),
+                                        text="Colour Name", width=15)
+            self.colour_button.grid(row=item // 2,
+                                    column=item % 2,
+                                    padx=5, pady=5)
+
+        # Frame to hold hints and stats buttons
+        self.hints_stats_frame = Frame(self.game_frame)
+        self.hints_stats_frame.grid(row=6)
+
+        # list for buttons (frame | text | bg | command | width | row | column)
+        control_button_list = [
+            [self.game_frame, "Next Round", "#0057D8", "", 21, 5, None],
+            [self.hints_stats_frame, "Hints", "#FF8000", "", 10, 0, 0],
+            [self.hints_stats_frame, "Stats", "#333333", "", 10, 0, 1],
+            [self.game_frame, "End", "#990000", self.close_play, 21, 7, None]
+        ]
+
+        # create buttons and add to list
+        control_ref_list = []
+        for item in control_button_list:
+            make_control_button = Button(item[0], text=item[1], bg=item[2], command=item[3], font=("Arial", 16, "bold"),
+                                         fg="#FFFFFF", width=item[4])
+            make_control_button.grid(row=item[5], column=item[6], padx=5, pady=5)
+
+            control_ref_list.append(make_control_button)
+
+    def close_play(self):
+        # reshow root (ie: choose rounds) and end current
+        # game / allow new game to start
+        root.deiconify()
+        self.play_box.destroy()
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Colour Quest")
+    StartGame()
+    root.mainloop()
--- C_03_get_all_colours.py
+++ C_03_get_all_colours.py
@@ -1,6 +1,16 @@
 import csv
 import random
 
+def rounds_ans(val):
+    """
+    Rounds numbers to nearest integer
+    :param val: number to be rounded.
+    :return: Rounded number (an integer)
+    """
+    var_rounded = (val * 2 + 1) // 2
+    raw_rounded = "{:.0f}".format(var_rounded)
+    return int(raw_rounded)
+
 # Retrieve colours from csv file and put them in a list
 file = open("00_colour_list_hex_v3.csv", "r")
 all_colours = list(csv.reader(file, delimiter=","))
@@ -8,4 +18,28 @@ file.close()
 
 #remove the first row
 all_colours.pop(0)
-print(all_colours)
+
+round_colours = []
+colour_scores = []
+
+# loop until we have four colours with different scores...
+while len(round_colours) < 4:
+    potential_colour = random.choice(all_colours)
+
+    # Get the score and check it's not a duplicate
+    if potential_colour[1] not in colour_scores:
+        round_colours.append(potential_colour)
+        colour_scores.append(potential_colour[1])
+
+print(round_colours)
+print(colour_scores)
+
+# find target score (median)
+
+# change scores to integers
+int_scores = [int(x) for x in colour_scores]
+int_scores.sort()
+
+median = (int_scores[1] + int_scores[2]) / 2
+print(f"unrounded median: {median}")
+print(f"median: {median:.0f}")
